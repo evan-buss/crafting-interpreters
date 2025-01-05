b5613c0bd05ccc8e2ec5982ce3f629457e9569c9
Evan Buss <evan.buss28@gmail.com>
2025-01-05 14:36:32 -0500

    feat: support string + number concatenation

--- src/main/java/com/evanbuss/Lox/Interpreter.java
+++ src/main/java/com/evanbuss/Lox/Interpreter.java
@@ -30,11 +30,12 @@ public class Interpreter implements Expr.Visitor<Object> {
                 if (left instanceof Double && right instanceof Double) {
                     return (double) left + (double) right;
                 }
-                if (left instanceof String && right instanceof String) {
-                    return (String) left + (String) right;
+
+                if (left instanceof String || right instanceof String) {
+                    return stringify(left) + stringify(right);
                 }
 
-                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
+                throw new RuntimeError(expr.operator, "Operands must be numbers or strings.");
             case GREATER:
                 checkNumberOperands(expr.operator, left, right);
                 return (double) left > (double) right;
